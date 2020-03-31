ecad1ee627fbb285d4d6550e4ade6fbf0143f1b7
Benjamin <Williams>
2020-03-31 14:30:18 -0400

    Initial commit

---
+++ src/main/java/org/wcci/googlecalendarapi/GoogleCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package org.wcci.googlecalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleCalendarApiApplication.class, args);
+    }
+
+}
